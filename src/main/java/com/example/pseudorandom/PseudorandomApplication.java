6765176122a071cc99ffb1003afc5098b9df4f24
hendraw4n <hendraw4n@gmail.com>
2021-12-25 04:36:50 +0000

    adding files created with spring boot generator

---
+++ src/main/java/com/example/pseudorandom/PseudorandomApplication.java
@@ -0,0 +1,13 @@
+package com.example.pseudorandom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PseudorandomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PseudorandomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pseudorandom/PseudorandomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pseudorandom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PseudorandomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
