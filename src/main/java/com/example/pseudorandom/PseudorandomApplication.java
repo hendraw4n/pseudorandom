afbae50777eef10a550fb25368f68fcda73a84a0
hendraw4n <hendraw4n@gmail.com>
2021-12-25 09:11:31 +0000

    added simple hello world web app

--- src/main/java/com/example/pseudorandom/PseudorandomApplication.java
+++ src/main/java/com/example/pseudorandom/PseudorandomApplication.java
@@ -1,10 +1,18 @@
 package com.example.pseudorandom;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PseudorandomApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PseudorandomApplication.class, args);
